7816ad01288c8da7b87ae96796f4a651831cad35
Rafael <leafar.huaman@gmail.com>
2014-04-10 00:06:27 +0000

    Add has_many :argument_posts to Debate

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,6 +1,7 @@
 class Debate < ActiveRecord::Base
   belongs_to :user
   belongs_to :chamber
+  has_many :argument_posts
   default_scope -> { order('created_at DESC') }
   validates :user_id, :chamber_id ,:content, :title, :affirmative, :negative , presence: true
   validates :content, length: { maximum: 5000 }
